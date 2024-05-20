e14a2781f8a0c1ca6dbfe56c040f70a3cd80f3d7
Aziz Önder <azizonder05@gmail.com>
2024-05-20 23:36:09 +0300

    Timer added from hüseytin

---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private float remainingTime;
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+            if (remainingTime > 90)
+            {
+                timerText.color = Color.white;
+            }
+            else if ((remainingTime < 90) && (remainingTime > 60))
+            {
+                timerText.color = Color.magenta;
+            }
+            else if ((remainingTime < 60) && (remainingTime > 30))
+            {
+                timerText.color = Color.red;
+            }
+        }
+        else if (remainingTime < 0)
+
+        {
+            remainingTime = 0;
+        }
+        //Dakika ve saniyenin 60 lý periyotlar halinde azalmasý 
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
